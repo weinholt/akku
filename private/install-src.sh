38c584dc0dcc11a3b1c35b6ca87259fa8e0ea34b
Lassi Kortela <lassi@lassi.io>
2019-04-01 23:40:30 +0300

    Add more command names for Chez Scheme
    The current tally is:
    
    Linux/OpenBSD:
    * scheme
    * scheme-script
    
    FreeBSD:
    * chez-scheme
    * chez-scheme-script
    
    MacOS:
    * chez
    * scheme-script

--- private/install-src.sh
+++ private/install-src.sh
@@ -5,10 +5,36 @@
 # This file is part of akku-*+src.tar.xz and installs a script wrapper
 # that starts Akku.scm.
 
+die()
+{
+    echo "$0: error: $*" >&2
+    exit 1
+}
+
 is_executable()
 {
     which "$1" >/dev/null 2>&1
 }
+
+chez_scheme_script=""
+for x in chez-scheme-script scheme-script; do
+    if is_executable "$x"; then
+        chez_scheme_script="$x"
+        break
+    fi
+done
+chez_scheme=""
+for x in chez-scheme chez scheme; do
+    if is_executable "$x"; then
+        chez_scheme="$x"
+        break
+    fi
+done
+if [ -n "$chez_scheme_script" ] && [ -z "$chez_scheme" ]; then
+    die "found $chez_scheme_script but not the Chez Scheme compiler"
+elif [ -n "$chez_scheme" ] && [ -z "$chez_scheme_script" ]; then
+    die "found $chez_scheme but not the Chez Scheme script interpreter"
+fi
 
 PREFIX=$HOME/.akku
 mkdir -p "$PREFIX"
@@ -22,7 +48,7 @@ cat > "$PREFIX/bin/akku.chezscheme" << EOF
 #!/bin/sh
 export CHEZSCHEMELIBDIRS="$PREFIX/lib"
 unset CHEZSCHEMELIBEXTS
-exec scheme-script "$PREFIX/bin/akku.sps" \$*
+exec $chez_scheme_script "$PREFIX/bin/akku.sps" \$*
 EOF
 
 cat > "$PREFIX/bin/akku.guile" << EOF
@@ -46,10 +72,11 @@ rm -f "$HOME/bin/akku"
 
 install_chez()
 {
-    chez="$1"
     export CHEZSCHEMELIBDIRS="$PREFIX/lib"
     unset CHEZSCHEMELIBEXTS
-    $chez --compile-imported-libraries --program "$PREFIX/bin/akku.sps" 2>/dev/null
+    $chez_scheme \
+        --compile-imported-libraries \
+        --program "$PREFIX/bin/akku.sps" 2>/dev/null
     ln -s "$PREFIX/bin/akku.chezscheme" "$HOME/bin/akku"
     echo You can now run '~/bin/akku'
 }
@@ -61,10 +88,8 @@ install_guile()
     echo You can now run '~/bin/akku' "(configured for GNU Guile 2.2+)"
 }
 
-if is_executable chez; then
-    install_chez chez
-elif is_executable scheme; then
-    install_chez scheme
+if [ -n "$chez_scheme" ]; then
+    install_chez
 else
     install_guile
 fi
